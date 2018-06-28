219965278795922432967251a9c540f90cc25e43
b3b0 <b3b0@users.noreply.github.com>
2018-06-28 18:46:40 -0500

    Update nsh.ps1

--- nsh.ps1
+++ nsh.ps1
@@ -12,8 +12,13 @@ $potentiallydangerousip = Read-Host "IP in question"
 
 if(Invoke-WebRequest https://www.abuseipdb.com/check/$potentiallydangerousip | Select-Object -ExpandProperty RawContent | select-string "was found in our database!")
 {
-    Write-Host "DANGER"
-    & 'C:\Program Files\Mozilla Firefox\firefox.exe' -new-tab -url https://www.abuseipdb.com/check/$potentiallydangerousip -new-tab -url https://www.threatcrowd.org/ip.php?ip=$potentiallydangerousip -new-tab -url https://www.virustotal.com/#/ip-address/$potentiallydangerousip -new-tab -url https://www.threatminer.org/host.php?q=$potentiallydangerousip
+    "DANGER
+    -------"
+    $answer = Read-Host "Will you launch a hunt?"
+    if($answer -eq "y" -or "yes)
+    {
+        & 'C:\Program Files\Mozilla Firefox\firefox.exe' -new-tab -url https://www.abuseipdb.com/check/$potentiallydangerousip -new-tab -url https://www.threatcrowd.org/ip.php?ip=$potentiallydangerousip -new-tab -url https://www.virustotal.com/#/ip-address/$potentiallydangerousip -new-tab -url https://www.threatminer.org/host.php?q=$potentiallydangerousip
+    {
 }
 else
 {
