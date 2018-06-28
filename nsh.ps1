0a64ae7ba742195371f51f72f32b07cc27ee4735
b3b0 <b3b0@users.noreply.github.com>
2018-06-28 15:52:43 -0500

    Create nsh.ps1

---
+++ nsh.ps1
@@ -0,0 +1,21 @@
+Clear-Host
+
+"
+---------------
+    N A N O
+  S N I P E
+    H U N T
+---------------
+"
+
+$potentiallydangerousip = Read-Host "IP in question"
+
+if(Invoke-WebRequest https://www.abuseipdb.com/check/$potentiallydangerousip | Select-Object -ExpandProperty RawContent | select-string "was found in our database!")
+{
+    Write-Host "DANGER"
+    & 'C:\Program Files\Mozilla Firefox\firefox.exe' -new-tab -url https://www.abuseipdb.com/check/$potentiallydangerousip -new-tab -url https://www.threatcrowd.org/ip.php?ip=$potentiallydangerousip -new-tab -url https://www.virustotal.com/#/ip-address/$potentiallydangerousip -new-tab -url https://www.threatminer.org/host.php?q=$potentiallydangerousip
+}
+else
+{
+    Write-Host "not danger"
+}
