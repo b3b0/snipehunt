ab145a10bfdeb8867fbd54e8c4398d015fd0dacd
b3b0 <b3b0@users.noreply.github.com>
2018-06-28 19:31:11 -0500

    Now for Windows and 'Nucks

--- nsh.ps1
+++ nsh.ps1
@@ -12,15 +12,23 @@ $potentiallydangerousip = Read-Host "IP in question"
 
 if(Invoke-WebRequest https://www.abuseipdb.com/check/$potentiallydangerousip | Select-Object -ExpandProperty RawContent | select-string "was found in our database!")
 {
-    "DANGER
-    -------"
+    Clear-Host
+    Write-Host "DANGER"
+    Write-Host "-------"
     $answer = Read-Host "Will you launch a hunt?"
-    if($answer -eq "y" -or "yes)
+    if($answer -eq "y")
+    {   
+        if($IsLinux -eq "True")
+        {
+            firefox -new-tab -url https://www.abuseipdb.com/check/$potentiallydangerousip -new-tab -url https://www.threatcrowd.org/ip.php?ip=$potentiallydangerousip -new-tab -url https://www.virustotal.com/#/ip-address/$potentiallydangerousip -new-tab -url https://www.threatminer.org/host.php?q=$potentiallydangerousip
+        }
+        else
+        {
+            & 'C:\Program Files\Mozilla Firefox\firefox.exe' -new-tab -url https://www.abuseipdb.com/check/$potentiallydangerousip -new-tab -url https://www.threatcrowd.org/ip.php?ip=$potentiallydangerousip -new-tab -url https://www.virustotal.com/#/ip-address/$potentiallydangerousip -new-tab -url https://www.threatminer.org/host.php?q=$potentiallydangerousip
+        }
+    }
+    else
     {
-        & 'C:\Program Files\Mozilla Firefox\firefox.exe' -new-tab -url https://www.abuseipdb.com/check/$potentiallydangerousip -new-tab -url https://www.threatcrowd.org/ip.php?ip=$potentiallydangerousip -new-tab -url https://www.virustotal.com/#/ip-address/$potentiallydangerousip -new-tab -url https://www.threatminer.org/host.php?q=$potentiallydangerousip
-    {
-}
-else
-{
-    Write-Host "not danger"
+        Write-Host "not danger"
+    }
 }
