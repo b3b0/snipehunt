ec7357c83fb9061ca56292d21560b1ec9bf1cd01
b3b0 <b3b0@users.noreply.github.com>
2020-01-09 10:21:14 -0600

    Moved to Chrome as default.
    If you use Firefox, just uncomment the line 26 and comment 25.

--- snipehunt-cli.ps1
+++ snipehunt-cli.ps1
@@ -2,9 +2,10 @@
 # https://github.com/b3b0
 
 Clear-Host
+Write-Host "|||||||||||||||||||||||||||||||||||||||||||||||||||||||||"
 Write-Host "SNIPEHUNT-CLI 2.0.1 - https://github.com/b3b0/snipehunt"
 Write-Host "|||||||||||||||||||||||||||||||||||||||||||||||||||||||||"
-$potentiallydangerousip = Read-Host "IP in question"
+$potentiallydangerousip = Read-Host "IP in question" -foregroundcolor Blue
 if(Invoke-WebRequest https://www.abuseipdb.com/check/$potentiallydangerousip -UseBasicParsing | Select-Object -ExpandProperty RawContent | select-string "was found in our database!")
 {
     Clear-Host
